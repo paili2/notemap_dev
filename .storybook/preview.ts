31db23a717f4d4f4660bbbd631e8f45ebe5d283e
paili2 <zxcv7502@naver.com>
2025-08-06 19:25:12 +0900

    style: 글로벌 배경 스타일 흰색으로 수정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,13 @@ import "../app/globals.css"; // Tailwind 글로벌 스타일
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: "plain-white",
+      values: [
+        { name: "plain-white", value: "#ffffff" },
+        { name: "dark", value: "#333333" },
+      ],
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
